730a5ca32ff9ef2bd1b3fe6766a395f96c77a923
Jacale <jannick.leismann@gmx.de>
2024-06-26 10:02:03 +0200

    project commit

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Position is required")]
+        public string Position { get; set; }
+    }
+}
